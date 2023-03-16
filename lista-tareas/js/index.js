83120fd9f337f382c572580c646d6f707c1947ce
Ezequiel Romero <eze.1003@gmail.com>
2023-03-16 10:40:16 -0300

    Code linting

--- lista-tareas/js/index.js
+++ lista-tareas/js/index.js
@@ -1,24 +1,24 @@
-const input = document.querySelector("input")
-const addButton = document.querySelector(".btn-add")
-const ul = document.querySelector("ul")
-const empty = document.querySelector(".empty")
-const priority = document.querySelector("#priority")
+const input = document.querySelector('input')
+const addButton = document.querySelector('.btn-add')
+const ul = document.querySelector('ul')
+const empty = document.querySelector('.empty')
+const priority = document.querySelector('#priority')
 
-addButton.addEventListener('click', (e) =>{
+addButton.addEventListener('click', (e) => {
   e.preventDefault()
 
   const taskName = input.value
 
-  if(taskName !== '') {
+  if (taskName !== '') {
     const li = document.createElement('li')
     const p = document.createElement('p')
     p.textContent = taskName
-    
-    if(priority.value === 'high') {
+
+    if (priority.value === 'high') {
       p.classList.add('high')
-    } else if(priority.value === 'medium') {
+    } else if (priority.value === 'medium') {
       p.classList.add('medium')
-    } else if(priority.value === 'low') {
+    } else if (priority.value === 'low') {
       p.classList.add('low')
     }
 
@@ -39,7 +39,7 @@ const addDeleteButton = () => {
     const itemToRemove = e.target.parentElement
     ul.removeChild(itemToRemove)
 
-    if(ul.childElementCount === 0) {
+    if (ul.childElementCount === 0) {
       empty.style.display = 'block'
     }
   })
