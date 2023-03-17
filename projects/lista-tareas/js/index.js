1f7365777c7523d4868ff165dfe0c0f4673af722
Ezequiel Romero <eze.1003@gmail.com>
2023-03-17 17:49:43 -0300

    Update task list

--- projects/lista-tareas/js/index.js
+++ projects/lista-tareas/js/index.js
@@ -1,8 +1,12 @@
 const input = document.querySelector('input')
 const addButton = document.querySelector('.btn-add')
-const ul = document.querySelector('ul')
 const empty = document.querySelector('.empty')
 const priority = document.querySelector('#priority')
+const prioritiesContainer = document.querySelector('.priorities-container')
+const highPriorityList = document.querySelector('.high-priority-list')
+const mediumPriorityList = document.querySelector('.medium-priority-list')
+const lowPriorityList = document.querySelector('.low-priority-list')
+const noPriorityList = document.querySelector('.no-priority-list')
 
 addButton.addEventListener('click', (e) => {
   e.preventDefault()
@@ -14,33 +18,46 @@ addButton.addEventListener('click', (e) => {
     const p = document.createElement('p')
     p.textContent = taskName
 
-    if (priority.value === 'high') {
-      p.classList.add('high')
-    } else if (priority.value === 'medium') {
-      p.classList.add('medium')
-    } else if (priority.value === 'low') {
-      p.classList.add('low')
-    }
-
     li.appendChild(p)
-    li.appendChild(addDeleteButton())
-    ul.appendChild(li)
+    li.appendChild(addDeleteButton(priority.value))
+
+    if (priority.value === 'high') {
+      highPriorityList.appendChild(li)
+    } else if (priority.value === 'medium') {
+      mediumPriorityList.appendChild(li)
+    } else if (priority.value === 'low') {
+      lowPriorityList.appendChild(li)
+    } else {
+      noPriorityList.appendChild(li)
+    }
 
     input.value = ''
     priority.value = ''
     empty.style.display = 'none'
+    prioritiesContainer.style.display = 'grid'
   }
 })
 
-const addDeleteButton = () => {
+const addDeleteButton = (priority) => {
   const delBtn = document.createElement('button')
   delBtn.textContent = 'x'
   delBtn.addEventListener('click', (e) => {
     const itemToRemove = e.target.parentElement
-    ul.removeChild(itemToRemove)
 
-    if (ul.childElementCount === 0) {
+    if (priority === 'high') {
+      highPriorityList.removeChild(itemToRemove)
+    } else if (priority === 'medium') {
+      mediumPriorityList.removeChild(itemToRemove)
+    } else if (priority === 'low') {
+      lowPriorityList.removeChild(itemToRemove)
+    } else {
+      noPriorityList.removeChild(itemToRemove)
+    }
+
+    const taskAmount = noPriorityList.childElementCount + lowPriorityList.childElementCount + mediumPriorityList.childElementCount + highPriorityList.childElementCount
+    if (taskAmount === 0) {
       empty.style.display = 'block'
+      prioritiesContainer.style.display = 'none'
     }
   })
   return delBtn
